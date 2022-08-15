0e187814b3cccbfd2dd8e38431b92f995941eefa
Alex <alex1031alex@yandex.ru>
2022-08-15 18:57:21 +0300

    Добавил и отрисовал компонент Player с хардкодными значениями пропсов

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,20 @@
 import "./app.scss";
-import {FC} from "react";
-import {NewGame} from "@components/new-game/new-game";
+import { FC } from "react";
+import { NewGame } from "@components/new-game/new-game";
+import { Player } from "@components/player/player";
 
 export const App: FC = () => {
     const newGameClickHandler = () => {};
     return (
         <div className="app">
             <h1 className="app__title">Game of dice</h1>
-            <div className="container">
+            <div className="container app__container">
                 <NewGame onClick={newGameClickHandler} />
-                <div className="app__playground"></div>
+                <div className="app__playground">
+                    <Player name="Player 1" isActive={true} currentScore={0} totalScore={0} />
+                    <div className="app__controls"></div>
+                    <Player name="Player 2" isActive={false} currentScore={0} totalScore={7} />
+                </div>
             </div>
         </div>
     );
---
+++ src/components/player/player.tsx
@@ -0,0 +1,22 @@
+import "./player.scss";
+import {FC} from "react";
+
+interface TProps  {
+    name: string;
+    isActive: boolean;
+    totalScore: number;
+    currentScore: number;
+}
+
+export const Player: FC<TProps> = ({name, isActive = true, totalScore, currentScore}) => {
+    return (
+        <div className="player">
+            <h2 className={`player__title ${isActive && "player__title--active"}`}>{name}</h2>
+            <p className="player__total-score">{totalScore}</p>
+            <div className="player__current-container">
+                <p className="player__current-label">current</p>
+                <p className="player__current-score">{currentScore}</p>
+            </div>
+        </div>
+    );
+}
