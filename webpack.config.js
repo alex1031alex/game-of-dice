92add2f24931008635244a13a9ad9049fc255ba4
Alex <alex1031alex@yandex.ru>
2022-08-15 13:44:55 +0300

    Настроил конфигурацию проекта

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "production",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        }]
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: "./src/index.html"
+    }),
+  ],
+  devServer: {
+    client: {
+      overlay: false,
+    },
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  }
+}
