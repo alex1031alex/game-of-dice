375ef20b57f8ad4083bb46aae7bd8cacdb78cc58
Alex <alex1031alex@yandex.ru>
2022-08-15 15:58:32 +0300

    Отрисовал компонент NewGame, добавил стили для компонента App

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,13 @@
-export const App = () => {
+import "./app.scss";
+import {FC} from "react";
+import {NewGame} from "@components/new-game/new-game";
+
+export const App: FC = () => {
+    const newGameClickHandler = () => {};
     return (
-        <div className="app">
-            <h1>Game of dice</h1>
+        <div className="app container">
+            <h1 className="app__title">Game of dice</h1>
+            <NewGame onClick={newGameClickHandler} />
         </div>
     );
 }
